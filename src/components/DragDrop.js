b541f032ad3b2d5c680e58bdd8269ae725b8f68a
nikhil andola <nikhilandola123@gmail.com>
2022-04-20 23:19:07 +0530

    added useDrag hook api

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -10,9 +10,9 @@ const ImageList = [
   },
   {
     id: 2,
-    alt: "astronaut lying in backyard",
+    alt: "Flamingo Bird",
     url:
-      "https://images.unsplash.com/photo-1626413715168-04a49227fe92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1522&q=80"
+      "https://images.unsplash.com/photo-1497206365907-f5e630693df0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
   },
   {
     id: 3,
--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 
 export const Picture = ({ url, id, alt }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "image",
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging()
+    })
+  }));
   return (
     <>
-      <img src={url} alt={alt} height="100px" width="100px" />
+      <img
+        ref={drag}
+        src={url}
+        alt={alt}
+        height="100px"
+        width="100px"
+        style={{ border: isDragging ? "5px solid pink" : "0px" }}
+      />
     </>
   );
 };
